49149144fc8c9d21dbf923b8383dc52bd22ccae3
Bang Nguyen <luongbang152@gmail.com>
2022-06-23 20:09:33 +0700

    redis 6.2.6 (new formula)
    Create redis.rb
    
    Closes #4.
    
    Signed-off-by: Bang Nguyen <luongbang152@gmail.com>

---
+++ Formula/redis.rb
@@ -0,0 +1,50 @@
+class Redis < Formula
+  desc "Persistent key-value database, with built-in net interface"
+  homepage "https://redis.io/"
+  url "https://download.redis.io/releases/redis-6.2.6.tar.gz"
+  sha256 "5b2b8b7a50111ef395bf1c1d5be11e6e167ac018125055daa8b5c2317ae131ab"
+  license "BSD-3-Clause"
+  head "https://github.com/redis/redis.git", branch: "unstable"
+
+  livecheck do
+    url "https://download.redis.io/releases/"
+    regex(/href=.*?redis[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/luongbang152/homebrew-brew/releases/download/redis-6.2.6"
+    sha256 cellar: :any,                 big_sur:      "9219cb2ad32149c70421793a837a08c872919fbb272130680ef04bc9affe60dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5dada42f07c7ed32c9bd84b51871557401489655e68126854cebb01c69fb2650"
+  end
+
+  depends_on "openssl@1.1"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}", "CC=#{ENV.cc}", "BUILD_TLS=yes"
+
+    %w[run db/redis log].each { |p| (var/p).mkpath }
+
+    # Fix up default conf file to match our paths
+    inreplace "redis.conf" do |s|
+      s.gsub! "/var/run/redis.pid", var/"run/redis.pid"
+      s.gsub! "dir ./", "dir #{var}/db/redis/"
+      s.sub!(/^bind .*$/, "bind 127.0.0.1 ::1")
+    end
+
+    etc.install "redis.conf"
+    etc.install "sentinel.conf" => "redis-sentinel.conf"
+  end
+
+  service do
+    run [opt_bin/"redis-server", etc/"redis.conf"]
+    keep_alive true
+    error_log_path var/"log/redis.log"
+    log_path var/"log/redis.log"
+    working_dir var
+  end
+
+  test do
+    system bin/"redis-server", "--test-memory", "2"
+    %w[run db/redis log].each { |p| assert_predicate var/p, :exist?, "#{var/p} doesn't exist!" }
+  end
+end
